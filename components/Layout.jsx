998a044e1b123f15840ac8fb19baff8d393007df
ikerpaster <ikerpaster@gmail.com>
2022-07-17 17:19:46 +0200

    update3

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Real Estates</title>
+        <title>Real Estate</title>
       </Head>
       <Box maxWidth="1280px" m="auto">
         <header>
